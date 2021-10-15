f9b32c4fe93f6090053d5aa57615212fbb3a5a8c
UmutSutcu <73347487+UmutSutcu@users.noreply.github.com>
2021-10-15 15:16:08 +0300

    Update proje.c

--- proje.c
+++ proje.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 int sabit = 50 ;
 
 struct kayit{
     int ogrno;
     int derskodu;
     int puan;
-
 };
-
 struct kayit sinif[100];
 FILE *ogrinfo;
-
 void indexDosyasiOlustur(){
     ogrinfo = fopen("veri.txt","w");
-
     for(int i= 0 ;i<sabit;i++){
         fprintf(ogrinfo,"%d.ogrencinin no: %d \n",i+1,sinif[i].ogrno);
         fprintf(ogrinfo,"%d.ogrencinin dersi: %d \n",i+1,sinif[i].derskodu);
@@ -22,29 +20,28 @@ void indexDosyasiOlustur(){
     }
     fclose(ogrinfo);
 }
+
 void indeksDosyasiniGoster(){
 
     ogrinfo = fopen("veri.txt","r");
-
     while(!feof(ogrinfo)){
         fputchar(fgetc(ogrinfo));
     }
-
     fclose(ogrinfo);
 
 }
+
 void kayitGuncelle(int i ){
     printf("%d.ogrencinin notu:%d ",i,sinif[i].puan);
     printf("\nYeni notunu giriniz: ");
     int yeni;
     scanf("%d",&yeni);
-
     sinif[i].puan=yeni;
     printf("Yeni notu: %d",sinif[i].puan);
-
     indexDosyasiOlustur();
 
 }
+
 void kayitEkle(){
     sabit++;
     printf("Yeni ogrencinin no: ");
@@ -53,38 +50,53 @@ void kayitEkle(){
     scanf("%d",&sinif[sabit].derskodu);
     printf("Yeni ogrencinin puani: ");
     scanf("%d",&sinif[sabit].puan);
-
     /*ogrinfo = fopen("veri.txt","a");
     fprintf(ogrinfo,"%d.ogrencinin no: %d \n",sabit,sinif[sabit].ogrno);
     fprintf(ogrinfo,"%d.ogrencinin dersi: %d \n",sabit,sinif[sabit].derskodu);
     fprintf(ogrinfo,"%d.ogrencinin notu:%d \n\n\n",sabit,sinif[sabit].puan);
     fclose(ogrinfo);*/
     indexDosyasiOlustur();
-
 }
 
 
+void indeksDosyasiniSil(){
+    int sonuc;
+
+    printf("Dosya silinecek! Devam edecek misiniz (E/H)?");
+    if (toupper(getche())=='E'){
+        sonuc = remove("veri.txt");
+
+    }
+    if(sonuc == 0){
+        printf("\n\nDosya basariyla silindi.");
+    }
+    else{
+        printf("Dosya silinemedi");
+    }
+
+
+
+}
+
 int main()
 {
-
-
         for(int i= 0 ;i<50;i++){
             sinif[i].ogrno=rand()%100;
             sinif[i].derskodu=rand()%3+1;
             sinif[i].puan=rand()%100;
+            sinif[i].puan=rand()%50+50;
         }
 
         int sec=1;
-
         while(sec !=0){
             printf("\n1-)index dosyasi olustur.\n");
             printf("2-)Kayit guncelle\n");
             printf("3-)Kayit ekle\n");
             printf("4-)indeks dosyasini goster\n");
+            printf("5-)indeks dosyasini siler\n");
             printf("0-)Cikis\n");
             printf("Birini seciniz lutfen: ");
             scanf("%d",&sec);
-
             switch (sec){
                 case 1:
                     indexDosyasiOlustur();
@@ -97,9 +109,13 @@ int main()
                     break;
                 case 3:
                     kayitEkle();
+                    break;
                 case 4:
                     indeksDosyasiniGoster();
-
+                    break;
+                case 5:
+                    indeksDosyasiniSil();
+                    break;
 
             }
 
