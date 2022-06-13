8360cb720d178676e78bf847139c106f3ab28528
UmutSutcu <73347487+UmutSutcu@users.noreply.github.com>
2022-06-13 11:11:32 +0300

    Create UserAuthenticationRegistery.java

---
+++ proje 6/Service/UserAuthenticationRegistery.java
@@ -0,0 +1,11 @@
+package service;
+
+import members.Customer;
+import members.Manager;
+import members.Representative;
+
+public class UserAuthenticationRegistery {
+    public static Customer REGISTERED_CUSTOMER;
+    public static Manager REGISTERED_MANAGER;
+    public static Representative REGISTERED_REPRESENTATIVE;
+}
